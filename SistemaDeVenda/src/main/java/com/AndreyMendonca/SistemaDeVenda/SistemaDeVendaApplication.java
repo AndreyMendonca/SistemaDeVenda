311366941793fdb9915eea7992897e41f35662fd
Andrey Mendonça <andrey.mmendonca@gmail.com>
2023-10-01 21:00:47 -0300

    Projeto inicial criado

---
+++ SistemaDeVenda/src/main/java/com/AndreyMendonca/SistemaDeVenda/SistemaDeVendaApplication.java
@@ -0,0 +1,13 @@
+package com.AndreyMendonca.SistemaDeVenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeVendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeVendaApplication.class, args);
+	}
+
+}
