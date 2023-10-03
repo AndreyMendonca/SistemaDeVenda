8eb12d0f67efda3f89fbcce9d130d60d4a097fa4
Andrey Mendonça <andrey.mmendonca@gmail.com>
2023-10-02 23:44:01 -0300

    Entitade Sale

---
+++ SistemaDeVenda/src/main/java/com/AndreyMendonca/SistemaDeVenda/entities/Sale.java
@@ -0,0 +1,54 @@
+package com.AndreyMendonca.SistemaDeVenda.entities;
+
+import java.time.Instant;
+import java.util.Objects;
+
+public class Sale {
+	private Long id;
+	private Instant date;
+	
+	public Sale() {
+		
+	}
+
+	public Sale(Long id, Instant date) {
+		super();
+		this.id = id;
+		this.date = date;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getDate() {
+		return date;
+	}
+
+	public void setDate(Instant date) {
+		this.date = date;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sale other = (Sale) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
